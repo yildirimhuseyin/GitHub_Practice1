ca5b4e3a1c2d3a2fe3401595dbc9b9953ad141c0
Hüseyin Yıldırım <11huseyinyildirim@gmail.com>
2022-12-09 15:01:04 +0100

    Created practice project and .gitignore file added

---
+++ src/Food.java
@@ -0,0 +1,14 @@
+public class Food {
+
+
+    public static void main(String[] args) {
+
+
+        // After water food is the second important supply of life
+
+        // while without water human can live 3 days, without food can live 40 days
+
+    }
+
+
+}
---
+++ src/Water.java
@@ -0,0 +1,15 @@
+public class Water {
+
+
+    public static void main(String[] args) {
+
+        // water is the main supply of life
+
+        // without water there is not be any life in the world
+
+
+
+
+    }
+
+}
